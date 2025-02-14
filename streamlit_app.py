7926790b3df8180cf596cd629140e67c7f1ec6a7
ArielSt95V2 <astablediffusion2@gmail.com>
2025-02-14 12:01:22 +0200

    initial setup

--- streamlit_app.py
+++
@@ -1,9 +0,0 @@
-# streamlit_app.py
-import subprocess
-import sys
-
-def launch_streamlit():
-    subprocess.run([sys.executable, "-m", "streamlit", "run", "app.py"])
-
-if __name__ == "__main__":
-    launch_streamlit()
